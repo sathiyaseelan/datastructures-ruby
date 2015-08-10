723c2f4210cbc2f687244122f43e96a47542c2d3
Sathiyaseelan <ssstarlet@gmail.com>
2015-08-10 17:47:35 +0530

    Update linked_list.rb
    Removed test code from source

--- list/linked_list.rb
+++ list/linked_list.rb
@@ -109,27 +109,3 @@ class LinkedList
 end
 
 end
-
-#Test code for linked list
-linked_list = List::LinkedList.new
-
-
-linked_list.add(5)
-linked_list.add(4)
-linked_list.add(3)
-linked_list.add(2)
-
-puts linked_list.has? 3
-puts linked_list.has? 2
-puts linked_list.has? 5
-puts linked_list.has? 1
-
-
-linked_list.add(2)
-linked_list.add(3)
-linked_list.add(2)
-linked_list.add(5)
-print linked_list
-linked_list.remove_all 2
-
-print linked_list
