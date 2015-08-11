7f7238e2e258a865bb4c9e2c78da6a74fbe7aaa3
Sathiyaseelan <ssstarlet@gmail.com>
2015-08-11 22:52:36 +0530

    Update DisjointSet.rb
    Added implementation for Disjoint set

--- src/disjoint-set/DisjointSet.rb
+++ src/disjoint-set/DisjointSet.rb
@@ -1,1 +1,53 @@
 
+# Disjoint set implementation based on weighted quick union and path compression
+module DisjointSet
+
+	class DisjointSet
+	
+		def initialize(n)
+			@ref = Array.new(n)
+			@size = Array.new(n)
+			(0...n).each do |i|
+				@ref[i] = i
+				@size[i] = 1
+			end
+		end
+		
+		public
+		
+		# To connect to elements in the set
+		def union a,b
+			i = root(a)
+			j = root(b)
+			if @size[i] < @size[j]
+				@ref[i] = j
+				@size[j]+=@size[i]
+			else
+				@ref[j] = i
+				@size[i]+=@size[j]
+			end
+		end
+		
+		# To check whether two elements are connected are not
+		def connected? a,b
+			if root(a) == root(b)
+				return true
+			else
+				return false
+			end
+		end
+		
+		private
+		
+		def root a
+			i = a
+			while (i!=@ref[i]) do
+				@ref[i] = @ref[@ref[i]]
+				i = @ref[i]
+			end
+			return i
+		end
+	
+	end
+end
+
